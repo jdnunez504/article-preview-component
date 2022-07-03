f9a2a7a169ed7389dc86f8f260d9c26a59493dd5
Joaquin Nunez <jdnun314@hotmail.com>
2022-07-02 21:51:58 -0500

    Desktop styles added

--- docs/js/menu.js
+++ docs/js/menu.js
@@ -1,16 +1,30 @@
 let menu = document.querySelector('.menu');
 let button = document.querySelector('.share');
 let buttonIcon = button.querySelector('svg path');
+let x = window.matchMedia("(min-width: 1100px)");
 
 button.addEventListener('click', function(e) {
     e.preventDefault();
-    if(menu.classList.contains('hidden') && !button.classList.contains('dark')) {
-        menu.classList.remove('hidden');
-        button.classList.add('dark');
-        buttonIcon.setAttribute('fill','#ffffff');
+    if (!x) {
+        if(menu.classList.contains('hidden') && !button.classList.contains('dark')) {
+            menu.classList.remove('hidden');
+            button.classList.add('dark');
+            buttonIcon.setAttribute('fill','#ffffff');
+        } else {
+            menu.classList.add('hidden');
+            button.classList.remove('dark');
+            buttonIcon.setAttribute('fill','#6E8098');
+        }
     } else {
-        menu.classList.add('hidden');
-        button.classList.remove('dark');
-        buttonIcon.setAttribute('fill','#6E8098');
+        if(menu.classList.contains('hidden') && !button.classList.contains('dark')) {
+            menu.classList.remove('hidden');
+            button.classList.add('dark');
+            buttonIcon.setAttribute('fill','#ffffff');
+        } else {
+            menu.classList.add('hidden');
+            button.classList.remove('dark');
+            buttonIcon.setAttribute('fill','#6E8098');
+        }
     }
+    
 })
