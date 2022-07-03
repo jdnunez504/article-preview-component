b90b47e739f83bd0e95bf5d1ab70e4337f8d5010
Joaquin Nunez <jdnun314@hotmail.com>
2022-07-02 21:56:18 -0500

    Changed menu.js

--- docs/js/menu.js
+++ docs/js/menu.js
@@ -5,26 +5,35 @@ let x = window.matchMedia("(min-width: 1100px)");
 
 button.addEventListener('click', function(e) {
     e.preventDefault();
-    if (!x) {
-        if(menu.classList.contains('hidden') && !button.classList.contains('dark')) {
-            menu.classList.remove('hidden');
-            button.classList.add('dark');
-            buttonIcon.setAttribute('fill','#ffffff');
-        } else {
-            menu.classList.add('hidden');
-            button.classList.remove('dark');
-            buttonIcon.setAttribute('fill','#6E8098');
-        }
+    // if (!x) {
+    //     if(menu.classList.contains('hidden') && !button.classList.contains('dark')) {
+    //         menu.classList.remove('hidden');
+    //         button.classList.add('dark');
+    //         buttonIcon.setAttribute('fill','#ffffff');
+    //     } else {
+    //         menu.classList.add('hidden');
+    //         button.classList.remove('dark');
+    //         buttonIcon.setAttribute('fill','#6E8098');
+    //     }
+    // } else {
+    //     if(menu.classList.contains('hidden') && !button.classList.contains('dark')) {
+    //         menu.classList.remove('hidden');
+    //         button.classList.add('dark');
+    //         buttonIcon.setAttribute('fill','#ffffff');
+    //     } else {
+    //         menu.classList.add('hidden');
+    //         button.classList.remove('dark');
+    //         buttonIcon.setAttribute('fill','#6E8098');
+    //     }
+    // }
+    if(menu.classList.contains('hidden') && !button.classList.contains('dark')) {
+        menu.classList.remove('hidden');
+        button.classList.add('dark');
+        buttonIcon.setAttribute('fill','#ffffff');
     } else {
-        if(menu.classList.contains('hidden') && !button.classList.contains('dark')) {
-            menu.classList.remove('hidden');
-            button.classList.add('dark');
-            buttonIcon.setAttribute('fill','#ffffff');
-        } else {
-            menu.classList.add('hidden');
-            button.classList.remove('dark');
-            buttonIcon.setAttribute('fill','#6E8098');
-        }
+        menu.classList.add('hidden');
+        button.classList.remove('dark');
+        buttonIcon.setAttribute('fill','#6E8098');
     }
     
 })
